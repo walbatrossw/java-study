9cf8ee58cd23f5caa711c14a323c7340cba066c5
SangSeob Yoon <walbatrossw@gmail.com>
2018-01-29 17:36:58 +0900

    9th commit : 컬렉션 프레임워크 - ArrayList 예제 작성중..

---
+++ standard-of-java/src/com/doubles/standardofjava/ch11_collections/part02_array_list/ArrayListEx2.java
@@ -0,0 +1,25 @@
+package com.doubles.standardofjava.ch11_collections.part02_array_list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx2 {
+    public static void main(String[] args) {
+        final int LIMIT = 10;
+        String source = "0123456789abcdefghiABCDEFGHIJ!@#$%^&*()ZZZ";
+        int length = source.length();
+
+        List list = new ArrayList(length/LIMIT + 10);
+
+        for (int i = 0; i < length; i+=LIMIT) {
+            if (i + LIMIT < length)
+                list.add(source.substring(i, i+LIMIT));
+            else
+                list.add(source.substring(i));
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
