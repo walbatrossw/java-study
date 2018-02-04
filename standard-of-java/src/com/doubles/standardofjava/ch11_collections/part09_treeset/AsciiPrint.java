2a4b0f15d49e815bc3bed230b9086b754f3130fd
SangSeob Yoon <walbatrossw@gmail.com>
2018-02-04 12:47:33 +0900

    22nd : commit : TreeSet 예제 연습

---
+++ standard-of-java/src/com/doubles/standardofjava/ch11_collections/part09_treeset/AsciiPrint.java
@@ -0,0 +1,10 @@
+package com.doubles.standardofjava.ch11_collections.part09_treeset;
+
+public class AsciiPrint {
+    public static void main(String[] args) {
+        char ch = ' ';
+        for (int i = 0; i < 95; i++) {
+            System.out.print(ch++);
+        }
+    }
+}
---
+++ standard-of-java/src/com/doubles/standardofjava/ch11_collections/part09_treeset/TreeSetEx1.java
@@ -0,0 +1,31 @@
+package com.doubles.standardofjava.ch11_collections.part09_treeset;
+
+import java.util.TreeSet;
+
+public class TreeSetEx1 {
+    public static void main(String[] args) {
+        TreeSet set = new TreeSet();
+
+        String from = "b";
+        String to = "d";
+
+        set.add("abc");
+        set.add("alien");
+        set.add("bat");
+        set.add("Car");
+        set.add("car");
+        set.add("disc");
+        set.add("dance");
+        set.add("dZZZZ");
+        set.add("dzzzz");
+        set.add("elephant");
+        set.add("elevator");
+        set.add("fan");
+        set.add("flower");
+
+        System.out.println(set);
+        System.out.println("range search from " + from + " to " + to);
+        System.out.println("result1 : " + set.subSet(from, to));
+        System.out.println("result1 : " + set.subSet(from, to + "zzz"));
+    }
+}
