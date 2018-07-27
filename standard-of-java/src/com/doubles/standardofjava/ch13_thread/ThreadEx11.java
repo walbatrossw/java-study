7266a97df9e7cf4d22f7044ea06d9856057d3c09
SangSeob Yoon <walbatrossw@gmail.com>
2018-07-27 22:56:30 +0900

    35th commit : thread 5

---
+++ standard-of-java/src/com/doubles/standardofjava/ch13_thread/ThreadEx11.java
@@ -0,0 +1,15 @@
+package com.doubles.standardofjava.ch13_thread;
+
+public class ThreadEx11 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class ThreadEx11_1 extends Thread {
+
+}
+
+class ThreadEx11_2 extends Thread {
+
+}
